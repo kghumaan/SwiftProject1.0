2fb518debbd16538a99d4b57878d1f8dccc382a9
Karanvir Singh Ghumaan <kghumaan@umail.iu.edu>
2017-05-27 13:42:33 -0400

    FB authentication works correctly, entitlements set as well...

--- TrynaMatchAPP/TrynaMatchAPP/ViewController.swift
+++ TrynaMatchAPP/TrynaMatchAPP/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,8 +17,21 @@ class ViewController: UIViewController {
         let loginButton = FBSDKLoginButton()
         view.addSubview(loginButton)
         loginButton.frame = CGRect(x: 16, y: 50, width: view.frame.width - 32, height: 50)
+        
+        loginButton.delegate = self
     }
-
-
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        print("Did log out of Facebook")
+    }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        if error != nil{
+            print(error)
+            return
+        }
+        
+        print("Successfully loged in with FB...")
+    }
 }
 
